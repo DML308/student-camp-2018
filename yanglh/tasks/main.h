be912504c76f7f9fe3753819b3fb79465fd14ac2
GabriellaMS <317148783@qq.com>
2019-08-19 08:30:02 +0800

    main

---
+++ yanglh/tasks/main.h
@@ -0,0 +1,26 @@
+#ifdef MAIN_H
+#define MAIN_H
+char* names[100];
+ine set[100];
+int count = 0;
+double value[100];
+
+//将变量名加入数组
+int add (char* name){
+    int i;
+    for(i=0;i<count;i++){
+        if(strcmp(name,names[i])==0){
+            return i;
+        }
+    }
+    count++;
+    name[i] = strdup(name);
+    return i;
+}
+//设置变量值
+int set(int index,double variable){
+    value[index] = variable;
+    set[index] = 1;
+    return variable;
+}
+#endif
